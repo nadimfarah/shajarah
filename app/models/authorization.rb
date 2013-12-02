1c4466f039279139ade3a64db810f3d925fe03f0
Nadim <nadimfarah@live.com>
2013-12-02 14:34:28 +0300

    heroku6

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -2,7 +2,7 @@ class Authorization < ActiveRecord::Base
 def self.find_or_create(auth_hash)
   unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     user = User.create :email => auth_hash["user_info"]["email"]
-    profile = Profile.create :User_id => user.User_id, relation: => "Self"
+
     auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
   end
  
