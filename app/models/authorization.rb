811d5cdc11a0942d837903527becfdba536ac22e
Nadim <nadimfarah@live.com>
2013-12-02 21:59:00 +0300

    User.new

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,7 +1,7 @@
 class Authorization < ActiveRecord::Base
 def self.find_or_create(auth_hash)
   unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    user = User.find(:id =>1)
+    user = User.new
     auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
   end
  
