df71ea7d903e245fa59bf977ad705558faed73ce
Nadim <nadimfarah@live.com>
2013-12-02 22:19:51 +0300

    auth del

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,12 +1,8 @@
 class Authorization < ActiveRecord::Base
 def self.find_or_create(auth_hash)
   unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    user = User.create 
-    auth = create :user_id => user.id, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+    auth = create :user_id => 1, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
   end
- 
-  auth
-
 end
 belongs_to :user
 validates :provider, :uid, :presence => true
