576da224b4d9bd0b505f9bcf89e6d6295e9ba401
Nadim <nadimfarah@live.com>
2013-12-02 20:47:59 +0300

    controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,21 @@ class SessionsController < ApplicationController
 
 def create
   auth_hash = request.env['omniauth.auth']
+  if session[:uid]
+    # Means our user is signed in. Add the authorization to the user
+    User.find(session[:uid]).add_provider(auth_hash)
  
-  render :text => auth_hash.inspect
+    render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+  else
+    # Log him in or sign him up
+    auth = Authorization.find_or_create(auth_hash)
+ 
+    # Create the session
+    session[:user_id] = auth.user.id
+ 
+    render :text => "Welcome #{auth.user.name}!"
+  end
+ 
 end
 
 def failure
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,11 +1,12 @@
 class Authorization < ActiveRecord::Base
 def self.find_or_create(auth_hash)
   unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    user = User.create :email => auth_hash["user_info"]["email"]
+    user = User.create 
     auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
   end
  
   auth
+
 end
 belongs_to :user
 validates :provider, :uid, :presence => true
