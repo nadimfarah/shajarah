ed80062e802cc1e61f49acb927029d6af4a4cb3c
Nadim <nadimfarah@live.com>
2013-12-02 16:12:03 +0300

    heroku1

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Shajarah
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-
-  end
     config.assets.initialize_on_precompile = false
+  end
+
 end
