8ed0eca49624a100fda3f3691c7b40e1193d8ade
Nadim <nadimfarah@live.com>
2013-12-03 09:54:17 +0300

    single provider

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,17 @@ class SessionsController < ApplicationController
 def create
   auth_hash = request.env['omniauth.auth']
  
-  if session[:user_id]
-    # Means our user is signed in. Add the authorization to the user
-    User.find(session[:user_id]).add_provider(auth_hash)
- 
-    render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+  @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+  if @authorization
+    render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
   else
-    # Log him in or sign him up
-    auth = Authorization.find_or_create(auth_hash)
+    user = User.new :email => auth_hash["info"]["email"]
+    user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+    user.save
  
-    # Create the session
-    session[:user_id] = auth.user.id
- 
-    render :text => "Welcome !"
+    render :text => "Hi #{user.name}! You've signed up."
   end
 end
-
 def failure
   render :text => "Sorry, but you didn't allow access to our app!"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ActiveRecord::Base
 has_many :profiles
  #has_secure_password
-def add_provider(auth_hash)
-  # Check if the provider already exists, so we don't add it twice
-  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    Authorization.create :user_id => self.id, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-  end
-end
+
 before_save { self.email = email.downcase }
   validates :email,
                     format:     { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
