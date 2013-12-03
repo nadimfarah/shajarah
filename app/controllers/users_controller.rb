b2217d3bbefb8210c3abd13149737018d7aaa954
Nadim <nadimfarah@live.com>
2013-12-03 10:22:41 +0300

    user.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    @user = User.new
+    @user = User.new :email => auth_hash["info"]["email"]
   end
 
   # GET /users/1/edit
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -8,4 +8,5 @@ def self.find_or_create(auth_hash)
 end
 belongs_to :user
 validates :provider, :uid, :presence => true
+attr_accessible :provider, :uid, :user_id, :user (ADD :user)
 end
