76f5396107cf656c105c2ce28b503f05b1392fde
Nadim <nadimfarah@live.com>
2013-12-03 08:55:49 +0300

    no unless

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,8 +1,8 @@
 class Authorization < ActiveRecord::Base
 def self.find_or_create(auth_hash)
-  unless auth == find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+
     auth = create :user_id => 1, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-  end
+
 end
 belongs_to :user
 validates :provider, :uid, :presence => true
