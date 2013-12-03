fa6f33f5f39086dab8bd92d03b9cc04b55072391
Nadim <nadimfarah@live.com>
2013-12-03 09:48:32 +0300

    add_provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 has_many :profiles
  #has_secure_password
-
+def add_provider(auth_hash)
+  # Check if the provider already exists, so we don't add it twice
+  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    Authorization.create :user_id => self.id, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+  end
+end
 before_save { self.email = email.downcase }
   validates :email,
                     format:     { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
@@ -9,11 +14,6 @@ before_save { self.email = email.downcase }
   has_secure_password
   validates :password, length: { minimum: 6 }
 end
-def add_provider(auth_hash)
-  # Check if the provider already exists, so we don't add it twice
-  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    Authorization.create (:user_id => self.id, :provider => auth_hash["provider"], :uid => auth_hash["uid"])
-  end
-end
+
 has_many :authorizations
 end
