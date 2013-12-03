cf310bf06205781f6cea7a8a33dce4c6cb72c2bd
Nadim <nadimfarah@live.com>
2013-12-03 09:44:50 +0300

    create

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ end
 def add_provider(auth_hash)
   # Check if the provider already exists, so we don't add it twice
   unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    Authorization.create :user_id => self.id, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+    Authorization.create (:user_id => self.id, :provider => auth_hash["provider"], :uid => auth_hash["uid"])
   end
 end
 has_many :authorizations
