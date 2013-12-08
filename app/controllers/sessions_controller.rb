8ed718bb588b374bffa86bf323f99370cec45254
Nadim <nadimfarah@live.com>
2013-12-08 15:00:54 +0300

    id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,10 @@ def create
   auth_hash = request.env['omniauth.auth']
  
   @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
- user1= User.new
+
+
+ lastid = User.last.id.to_i + 1
+ user1= User.create(:id => lastid )
  user1.save 
  @authorization.user_id = user1.id
 
