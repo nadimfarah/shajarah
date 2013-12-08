fd98cb39662e7176fa44698a2fe0ba78cd24bba0
Nadim <nadimfarah@live.com>
2013-12-08 11:16:59 +0300

    redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ def create
  
   @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   if @authorization.user_id
+    redirect_to :controller=>'uses', :action => 'show', :id => @authorization.user_id
   else
  user= User.create
  @authorization.user_id =user.id
-redirect_to :controller=>'uses', :action => 'show', :id => @authorization.user_id
-
+    redirect_to :controller=>'uses', :action => 'show', :id => @authorization.user_id
   end
 end
 def failure
