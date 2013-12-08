23d85162e8ef2dcd4decd3ffae5f80d6096e0b4a
Nadim <nadimfarah@live.com>
2013-12-08 14:11:24 +0300

    last

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,10 @@ def create
   @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   if @authorization.user_id
     render  :text => @authorization.user_id
-    
 
   else
-    lastid = User.last.id  +1
- user1= User.new(:id => lastid)
- @authorization.user_id =user1.id
- redirect_to :controller => "users", :action => "show", :id =>  @authorization.user_id
+ 
+ redirect_to :controller => "users", :action => "show", :id =>  User.last.id
   end
 end
 def failure
