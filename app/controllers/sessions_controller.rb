0c0d28bce9e772c096d1b17dbf51eeb6bc257ac6
Nadim <nadimfarah@live.com>
2013-12-08 14:50:03 +0300

    create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ def create
 
 
  lastid = User.last.id.to_i + 1
- user1= User.new(:id => lastid)
+ user1= User.create(:id => lastid )
  @authorization.user_id = user1.id
 
    redirect_to :controller => "users", :action => "show", :id =>  @authorization.user_id
