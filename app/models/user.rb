fb4f8e0fbc9cefbf5c4e4a8c2e1659720b52baed
Nadim <nadimfarah@live.com>
2013-12-10 16:51:29 +0300

    don't down case

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 has_many :profiles
  #has_secure_password
 
-before_save { self.email = email.downcase }
   validates :email,
                     format:     { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
