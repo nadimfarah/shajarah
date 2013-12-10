c181332f9cd738a992934053243869d3b8721489
Nadim <nadimfarah@live.com>
2013-12-10 16:45:12 +0300

    don't validate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,9 @@ def create
  lastid = User.last.id.to_i + 1
  user1= User.new
  user1.id= lastid
- user1.save
+ user1.save(:validate => false)
  @authorization.user_id = user1.id
- 
+
 
    redirect_to :controller => "users", :action => "show", :id => user1.id
 end
