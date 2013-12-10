523a82fa6d9a736880097be39555a2f3ef50b18f
Nadim <nadimfarah@live.com>
2013-12-10 19:31:30 +0300

    if user exists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,17 @@ def create
  
   @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   email = auth_hash["extra"]["raw_info"]["email"]
+  if (User.find_by_email(email).exists?){ 
+    user = User.find_by_email(email)
+    @authorization.user_id = user.id}
+  else {
  lastid = User.last.id.to_i + 1
  user1= User.new
  user1.id= lastid
  user1.email = email
  user1.save(:validate => false)
- @authorization.user_id = user1.id
+ @authorization.user_id = user1.id}
+
 
 
    redirect_to :controller => "users", :action => "show", :id => user1.id
