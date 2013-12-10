b15779639ddb39718c70b96e006de3b902d5b80a
Nadim <nadimfarah@live.com>
2013-12-10 22:05:46 +0300

    else

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,24 @@ class SessionsController < ApplicationController
   end
 
 def create
+
+  if params[:session][:email]
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+          sign_in user
+      redirect_to user
+  else
+      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+  end
+  else 
   auth_hash = request.env['omniauth.auth']
- 
   @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   email = auth_hash["extra"]["raw_info"]["email"]
   usertest = User.find_by_email(email)
   if usertest
     @authorization.user_id = usertest.id
-    
+
 
   else 
  lastid = User.last.id.to_i + 1
@@ -20,7 +30,8 @@ def create
  user1.save(:validate => false)
  @authorization.user_id = user1.id
 end
-
+end
+session
    redirect_to :controller => "users", :action => "show", :id => @authorization.user_id
 end
 def failure
