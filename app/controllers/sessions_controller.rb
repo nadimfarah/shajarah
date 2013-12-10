9d74ad0d71165c18f837adccc6da7540c3f105ed
Nadim <nadimfarah@live.com>
2013-12-10 22:16:58 +0300

    check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
 def create
-
-  if params[:session][:email]
+checkemail = params[:session][:email]
+  if checkemail
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
           sign_in user
