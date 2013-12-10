f8f3c47c26785fa023c080bd4be1583fa868b5a1
Nadim <nadimfarah@live.com>
2013-12-10 21:58:50 +0300

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Shajarah::Application.routes.draw do
   resources :profiles
   resources :users
+  resources :sessions
 root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
