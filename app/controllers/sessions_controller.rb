75c7d563fd4da9255a3011ab2327c736140a4e12
Nadim <nadimfarah@live.com>
2013-12-10 23:11:27 +0300

    id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user.password == params[:session][:password]
           sign_in user
-      redirect_to user
+   redirect_to :controller => "users", :action => "show", :id => user.id
     else
       flash[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
