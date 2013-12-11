74de6c89eab71bee181a28e27f0213bd285a1400
Nadim <nadimfarah@live.com>
2013-12-11 10:31:53 +0300

    wlc back

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ def create
   if params[:session]
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-          
+          flash[:error] = 'Welcome Back' # Not quite right!
           redirect_to :controller => "users", :action => "show", :id => user.id
     else
       flash[:error] = 'Invalid email/password combination' # Not quite right!
