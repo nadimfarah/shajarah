a42ae938ff1fb8f6d7d2f6eaf96fb50e12aa6827
Nadim <nadimfarah@live.com>
2013-12-12 13:09:24 +0300

    destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -38,9 +38,7 @@ def failure
   render :text => "Sorry, but you didn't allow access to our app!"
 end
 def destroy
-  if session[:user_id]
-    session[:user_id] = nil
-  end
+
  redirect_to root_path
 end
 end
