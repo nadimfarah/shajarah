0834444d16f7783976785ff28f1b3ae1f7c00bc4
Nadim <nadimfarah@live.com>
2013-12-12 12:37:35 +0300

    match

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Shajarah::Application.routes.draw do
   match '/signup', to: 'users#new', via: 'get'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   get   '/login', :to => 'sessions#new', :as => :login 
-  match 'signout', to: 'sessions#destroy', :as => :signout, via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', via: [:get, :post]
   get '/users', :to => 'users#index'
    get '/users/:id', :to => 'users#show'
   
