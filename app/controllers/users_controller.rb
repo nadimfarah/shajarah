1d8e40e62a494bb16a9ad082a5eceb958e886550
Nadim <nadimfarah@live.com>
2013-12-14 17:48:33 +0300

    sign in on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         flash[:success] = "Welcome to Shajarah!"
+             sign_in(@user)
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
