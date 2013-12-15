5d5f46d1cfed7b02e8fb9876a995fca880cad36c
Nadim <nadimfarah@live.com>
2013-12-15 16:09:29 +0300

    Revert "iframe"
    This reverts commit 8c7df05e144f53375c2a12ec0026c6e8b981f85c.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "786610081354504", "74acca43387246d638424aee5edd4911", :iframe => "true"
+  provider :facebook, "786610081354504", "74acca43387246d638424aee5edd4911"
 end
