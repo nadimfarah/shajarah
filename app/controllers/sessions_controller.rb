c714c2e2bac9f6a5c2660809b670fb7e8f310de4
Nadim <nadimfarah@live.com>
2013-12-15 17:09:04 +0300

    fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,14 +18,10 @@ def create
     end
   else 
    auth_hash = request.env['omniauth.auth']
-    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     email = auth_hash["extra"]["raw_info"]["email"]
     usertest = User.find_by_email(email)
     if usertest
-      if @authorization
-        @authorization.user_id = usertest.id
-      end
-
       sign_in(usertest)
     else 
       lastid = User.last.id.to_i + 1
@@ -33,10 +29,9 @@ def create
       user1.id= lastid
       user1.email = email
       user1.save(:validate => false)
-      @authorization.user_id = user1.id
-      @authorization.save
       sign_in(user1)
     end
+
     redirect_to :controller => "users", :action => "show", :id => @authorization.user_id
 end
 session
