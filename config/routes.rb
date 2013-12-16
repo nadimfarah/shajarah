dc05f2b7d22e46bb96f7b277a8d18580661f1aa1
Nadim <nadimfarah@live.com>
2013-12-16 23:59:51 +0300

    h

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Shajarah::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup', to: 'users#new', via: 'get'
+  match '/signup', to: 'users#new', via: [:get, :post]
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   get   '/login', :to => 'sessions#new', :as => :login 
   get   '/logout', :to => 'sessions#destroy', :as => :logout 
