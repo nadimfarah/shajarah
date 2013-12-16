d947a46f208117ed3a0e82d6a67e09ddf379a344
Nadim <nadimfarah@live.com>
2013-12-16 21:40:20 +0300

    route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Shajarah::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/authorize',  to: 'static_pages#authorize', via: 'post'
   match '/signup', to: 'users#new', via: 'get'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: 'auth/:provider/callback', via: [:get, :post]
@@ -13,7 +14,7 @@ Shajarah::Application.routes.draw do
   get   '/logout', :to => 'sessions#destroy', :as => :logout 
   get '/users', :to => 'users#index'
    get '/users/:id', :to => 'users#show'
-  match '/authorize',    to: 'static_pages#authorize', via: [:get, :post]
+
 
   
   # The priority is based upon order of creation: first created -> highest priority.
