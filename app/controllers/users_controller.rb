3d74192a79cb37b53d136c960ae084fbccbc7d62
Nadim <nadimfarah@live.com>
2013-12-16 14:28:43 +0300

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,8 +36,7 @@ class UsersController < ApplicationController
         user1 = User.find_by_email(user_params[:email])
         authent = Authorization.find_by_user_id(user1.id)
         if authent
-          user1.password, user1.password_confirmation = user_params[:password]
-          user1.save
+          user1.update(user_params)
           flash[:success] = "Welcome Back, You already have an account connected to facebook!"
         format.html { redirect_to "/auth/facebook/" }
         format.json { render action: 'show', location: @user }
