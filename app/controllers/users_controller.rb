fff73fcc1a9b4f459ffaec94003a6d6694aa4cf4
Nadim <nadimfarah@live.com>
2013-12-16 12:44:41 +0300

    user1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
         format.json { render action: 'show', status: :created, location: @user }
       else
         user1 = User.find_by_email(@user.email) 
-        if user1.authorizations.first
+        if user1
           flash[:success] = "Welcome Back, You already have an account connected to facebook!"
           user1.password,user1.password_confirmation = user_params[:password]
           user1.save
