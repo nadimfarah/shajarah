69b311aaa62db6bd13954b65fa43ed2f158b9915
Nadim <nadimfarah@live.com>
2013-12-16 10:48:10 +0300

    no user id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,6 @@ def create
     email = auth_hash["extra"]["raw_info"]["email"]
     usertest = User.find_by_email(email)
     if usertest
-      @authorization.user_id = usertest.id
       sign_in(usertest)
     else 
       lastid = User.last.id.to_i + 1
