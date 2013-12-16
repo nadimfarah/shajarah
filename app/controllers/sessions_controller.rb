9f6b5cb180a936556a8faa3017112fe580b46569
Nadim <nadimfarah@live.com>
2013-12-16 11:58:33 +0300

    create pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,6 +28,7 @@ def create
       user1= User.new
       user1.id= lastid
       user1.email = email
+      user1.password, user1.password_confirmation = SecureRandom.hex(9)
       user1.save(:validate => false)
       @authorization.user_id = user1.id
       @authorization.save
