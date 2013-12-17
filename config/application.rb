7aeffe51d3824409f79a739698e2e3bda20eb311
Nadim <nadimfarah@live.com>
2013-12-17 14:57:42 +0300

    allow

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module Shajarah
     # config.i18n.default_locale = :de
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.initialize_on_precompile = false
-    config.action_dispatch.default_headers[:'X-Frame-Options'] = "ALLOW-FROM https://apps.facebook.com"
+    config.action_dispatch.default_headers[:'X-Frame-Options'] = "ALLOW"
   end
 end
